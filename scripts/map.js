da350d476abc6a777b26cb76dd1350f75dd7ac8d
Andressa Lima <lima.andressab@outlook.com>
2023-06-23 16:19:02 -0300

    insercao do mapa

---
+++ scripts/map.js
@@ -0,0 +1,8 @@
+
+/* MAP */
+var map = L.map('map').setView([-23.5489, -46.6388], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
